5262ced42cc60ac3031d454a4b9e01a194f9db91
Jaeho Cho <jaeho8032@gmail.com>
2018-08-13 16:23:26 +0900

    소프트웨어학부 게시판 크롤러 구현

---
+++ sw_board_crawling.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+#파일의 위치
+#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = '/Users/jaeho/develop/soongsil-swbot'
+
+html = requests.get('http://sw.ssu.ac.kr/bbs/board.php?bo_table=sub6_1')
+soup = BeautifulSoup(html.text, 'html.parser')
+posts = soup.select('.board_list .subject')
+latest = posts[0].text
+
+with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+') as f_read:
+    before = f_read.readline()
+    if before != latest:
+        '''
+        모든 구독자에게 새 글 알림을 보낸다.
+        '''
+        pass
+    f_read.close()
+
+with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
+    f_write.write(latest)
+    f_write.close()
+
+'''
+link = 'http://sw.ssu.ac.kr' + posts[0].find('a').get('href')[2:]
+title = posts[0].text.strip()
+post = {
+    'link': link,
+    'title': title,
+}
+'''
