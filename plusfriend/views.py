5ff9b6a60860a7caff6c9978154268341c93fb72
Jaeho Cho <jaeho8032@gmail.com>
2018-08-26 15:49:35 +0900

    plusfriend/views.py 기본 구조 설정

--- plusfriend/views.py
+++ plusfriend/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from .decorators import bot
 
-# Create your views here.
+@bot
+def on_init(request):
+    return {'type': 'text'}
+
+
+@bot
+def on_message(request):
+    pass
+
+@bot
+def on_added(request):
+    user_key = request.JSON['user_key']
+
+
+@bot
+def on_block(request, user_key):
+    pass
+
+
+@bot
+def on_leave(request, user_key):
+    pass
