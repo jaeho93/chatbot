4fa54ca56f33f93807929a55b9cef30a03823b2a
Jaeho Cho <jaeho8032@gmail.com>
2018-08-26 16:11:50 +0900

    Subscription 모델 설계

--- plusfriend/models.py
+++ plusfriend/models.py
@@ -1,3 +1,7 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
